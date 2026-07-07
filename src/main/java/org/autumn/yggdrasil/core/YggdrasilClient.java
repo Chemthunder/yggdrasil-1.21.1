2441cbd01a1b01f4aa2d1e40604c262f93dcc951
Chemthunder <chemthunderisme@gmail.com>
2026-07-06 22:41:57 -0400

    ajfobn

--- src/main/java/org/autumn/yggdrasil/core/YggdrasilClient.java
+++ src/main/java/org/autumn/yggdrasil/core/YggdrasilClient.java
@@ -1,14 +1,22 @@
 package org.autumn.yggdrasil.core;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
 import net.fabricmc.fabric.api.client.rendering.v1.WorldRenderEvents;
+import org.autumn.yggdrasil.core.event.UnworthyEvent;
 import org.autumn.yggdrasil.core.event.YggRenderEvent;
 import org.autumn.yggdrasil.core.index.ModModelLayers;
 
 public class YggdrasilClient implements ClientModInitializer {
+    public static int GLOBAL_AGE = 0;
+
     public void onInitializeClient() {
         ModModelLayers.clientInit();
 
         WorldRenderEvents.LAST.register(new YggRenderEvent());
+//        HudRenderCallback.EVENT.register(new UnworthyEvent());
+
+        ClientTickEvents.START_CLIENT_TICK.register(minecraftClient -> GLOBAL_AGE++);
     }
 }
